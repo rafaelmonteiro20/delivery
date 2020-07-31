57afe555987fff26e028bb12a3794301c6473491
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-07-30 21:54:48 -0300

    Criando projeto delivery-store

---
+++ delivery-store/src/main/java/com/delivery/store/DeliveryStoreApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryStoreApplication.class, args);
+	}
+
+}
